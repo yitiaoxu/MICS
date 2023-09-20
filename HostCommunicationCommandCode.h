db8e8f11ca96d2b05158e235873cb8822045445e
Xiao Siyu <xiaosiyu0603@qq.com>
2023-09-20 21:30:15 +0800

    补充一些常数。

--- HostCommunicationCommandCode.h
+++ HostCommunicationCommandCode.h
@@ -14,6 +14,9 @@
 #define HC_CMD_Specify64kBlock			0x21	// 指定64K块编号
 #define HC_CMD_Output64kBlock			0x22	// 导出当前64K块
 #define HC_CMD_Erase64kBlock			0x23	// 擦除当前64K块
+#define HC_CMD_Specify4kSector			0x24	// 指定4K扇区编号
+#define HC_CMD_Output4kSector			0x25	// 导出当前4K扇区（预留）
+#define HC_CMD_Erase4kSector			0x26	// 擦除当前4K扇区
 #define HC_CMD_AskLastWriteResult		0x28	// 询问上次写入是否出错
 #define HC_CMD_AskFlashStatus			0x29	// 询问Flash是否忙
 #define HC_CMD_WriteFlashAtAddr			0x2a	// 在指定地址写入数据
