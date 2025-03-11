f0528a6c1f755a6e9269225199e3db64a05d6c91
Xiao Siyu <xiaosiyu0603@qq.com>
2025-03-12 01:03:04 +0800

    修改文档，未完成。

--- doc/上位机发送命令、从机接收数据并响应.js
+++
@@ -1,9 +0,0 @@
-{
-	signal: [
-		{ name: "time", wave: "xPP|PPPPPPPPPPPP|PPPPP", },
-		{ name: "master", wave: "137|1.|45..8.42.|41...", data: ["ENQ", "cmd      ", "SOH", "(optional args)", "length", "STX", "data", "ETX",] },
-		{ name: "slave (success)", wave: "1..|.41.........|..41.", data: ["ACK", "ACK",] },
-		{ name: "slave (handshake fail)", wave: "1..|.491........|.....", data: ["NAK", "ERR",] },
-		{ name: "slave (protocol error)", wave: "1..|.41.........|..491", data: ["ACK", "NAK", "ERR",] },
-	]
-}
